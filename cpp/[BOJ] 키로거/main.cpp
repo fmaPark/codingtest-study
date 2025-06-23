b17b338917f285ad61723ce847294d737b7d9d8e
mapark <alsdkdi0908@gmail.com>
2025-06-23 12:59:28 +0900

    [BOJ] 키로거

---
+++ cpp/[BOJ] 키로거/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <list>
+using namespace std;
+
+int main(void) {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        string input;
+        cin >> input;
+
+        list<char> pwd;
+        list<char>::iterator cur = pwd.begin();
+
+        for (char c: input) {
+            if (c == '<') {
+                if (cur != pwd.begin()) {
+                    cur--;
+                }
+            } else if (c == '>') {
+                if (cur != pwd.end()) {
+                    cur++;
+                }
+            } else if (c == '-') {
+                if (cur != pwd.begin()) {
+                    cur--;
+                    cur = pwd.erase(cur);
+                }
+            } else {
+                cur = pwd.insert(cur, c);
+                cur++;
+            }
+        }
+
+        for (char c: pwd) {
+            cout << c;
+        }
+        cout << '\n';
+    }
+    
+
+    return 0;
+}
