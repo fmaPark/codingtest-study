2415fa8d94a0f57052dce6f232514b36e8804332
Min-Ah Park <68277847+fmaPark@users.noreply.github.com>
2026-01-31 19:38:21 +0900

    [Bronze III] Title: 최소, 최대, Time: 320 ms, Memory: 129196 KB -BaekjoonHub

---
+++ Python/백준/Bronze/10818. 최소， 최대/최소， 최대.py
@@ -0,0 +1,13 @@
+t = input()
+arr = list(map(int, input().split()))
+
+min = arr[0]
+max = arr[0]
+
+for i in range(0, len(arr)):
+    if arr[i] < min:
+        min = arr[i]
+    if arr[i] > max:
+        max = arr[i]
+
+print(min, max)
